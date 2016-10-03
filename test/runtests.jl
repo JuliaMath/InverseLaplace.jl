1a4e2f544a2d5130ca6c044bff8f3e546248d450
John Lapeyre <lapeyre.math122a@gmail.com>
2016-10-03 17:47:07 +0200

    fix 32 bit floating error

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,12 +8,14 @@ using Base.Test
 @test_approx_eq( ilt(s -> s/(1+s^2),1) , cos(1))
 @test_approx_eq( ilt(s -> 1/(1+s),1) , exp(-1))
 @test_approx_eq( gwr(s -> 1/(1+s),1) , exp(-1))
-@test_approx_eq( ilt(s -> 1/s^4,1//10) * 6 , 1e-3)
-@test_approx_eq( gwr(s -> 1/s^4,1//10) * 6 , 0.0010000012595365085)
-
+if Int != Int32
+    @test_approx_eq( ilt(s -> 1/s^4,1//10) * 6 , 1e-3)
+    @test_approx_eq( gwr(s -> 1/s^4,1//10) * 6 , 0.0010000012595365085)
+    @test_approx_eq( ilt(s -> 1/s^3,[1//10,2//10]) , [2882303761517117//576460752303423488, 5764607523035027//288230376151711744])    
+else
+    @test_approx_eq_eps( ilt(s -> 1/s^4,1//10) * 6 , 1e-3,  1e-9)
+    @test_approx_eq_eps( gwr(s -> 1/s^4,1//10) * 6 , 0.0010000012595365085, 1e-9)
+end
 @test_approx_eq( ilt(s -> 1/s^3,[.1,.2]) , [0.005,0.02])
 @test_approx_eq( ilt(s -> 1/s^3,[.1,.2]) , [0.005,0.02])
 @test_approx_eq( map(x -> convert(Float64,x) , ilt(s -> 1/s^3,[BigFloat(1//10) , BigFloat(2//10)])), [0.005,0.02])
-if Int != Int32
-    @test_approx_eq( ilt(s -> 1/s^3,[1//10,2//10]) , [2882303761517117//576460752303423488, 5764607523035027//288230376151711744])
-end
