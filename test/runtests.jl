9be313ebe6d408122de33f5b3bebb1e27d406761
John Lapeyre <lapeyre.math122a@gmail.com>
2016-10-03 18:43:26 +0200

    skip some tests for v0.4

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using Base.Test
 @test_approx_eq( ilt(s -> s/(1+s^2),1) , cos(1))
 @test_approx_eq( ilt(s -> 1/(1+s),1) , exp(-1))
 @test_approx_eq( gwr(s -> 1/(1+s),1) , exp(-1))
-if Int != Int32
+if Int != Int32 && VERSION >= v"0.5.0"
     @test_approx_eq( ilt(s -> 1/s^4,1//10) * 6 , 1e-3)
     @test_approx_eq( gwr(s -> 1/s^4,1//10) * 6 , 0.0010000012595365085)
     @test_approx_eq( ilt(s -> 1/s^3,[1//10,2//10]) , [2882303761517117//576460752303423488, 5764607523035027//288230376151711744])    
