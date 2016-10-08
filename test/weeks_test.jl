af46c587760b4d8a5b9ddb573e98de57201299f5
John Lapeyre <lapeyre.math122a@gmail.com>
2016-10-08 19:01:33 +0200

    check. changing WeeksErr

--- test/weeks_test.jl
+++ test/weeks_test.jl
@@ -17,6 +17,11 @@ optimize(fl,10.0)
 e2 = fl(10.0) - cos(10.0)
 @test e2 < e1
 
+e1 = fle(10.0)[1] - cos(10.0)
+optimize(fle,10.0)
+e2 = fl(10.0)[1] - cos(10.0)
+@test e2 < e1
+
 fle = WeeksErr( s -> s/(1+s^2),  64 )
 (t1,e1) = fle(10.0)
 @test_approx_eq_eps([t1,e1], [-0.8390715290763104,1.6069233509475447e-11], 1e-9)
